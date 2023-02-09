f3f34fc338a391489f5e1f9d8d6bcf4918e0d1ad
bernalqz <bernalqz@gmail.com>
2023-02-09 17:26:03 -0600

    Master

--- views/footer.php
+++ views/footer.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
+
      <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
