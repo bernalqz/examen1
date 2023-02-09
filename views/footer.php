bff8b4a6b0cc22711b8394f36bcb5ac4ed01d1c6
bernalqz <bernalqz@gmail.com>
2023-02-09 17:42:00 -0600

    sese

--- views/footer.php
+++ views/footer.php
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 
-     <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
-    </footer>
-        <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
-</body>
-=======
         <!-- Footer-->
         <footer class="py-5 bg-dark">
             <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
@@ -19,5 +8,4 @@
             <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
->>>>>>> 121aa888a289f85a5059e8a60a28a6a12c3a1296
 </html>
