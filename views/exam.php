e12481ecde3661b0918d8765f191e6b61fc6c6c0
bernalqz <bernalqz@gmail.com>
2023-02-09 20:15:00 -0600

    ssss

--- views/exam.php
+++ views/exam.php
@@ -7,66 +7,16 @@
 
 
             <!-- Call to Action-->
-            <h1 class="font-weight-light text-center">CRUD</h1>
+            <h1 class="font-weight-light text-center">Ingresar Estudiante</h1>
            
            
 
-        
-    
-           
-            <div class="col-md-4 mb-5">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Agregar</h2>
-                            
 
 
-          <!-- Formulario-->
 
+  <!-- formulario-->          
 
-          <form>
-
-<div>   
-    
-
-<div class="container">
-  <div class="abs-center">
-<div class="form-group">
-  <label for="exampleInputEmail1">Email address</label>
-  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-</div>
-<div class="form-group">
-  <label for="exampleInputPassword1">Password</label>
-  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-</div>
-<div class="form-check">
-  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-</div>
-<button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-</div>
-</div>
-</div>
-
-         
-
-
-                        </div>
-                        <div class="card-footer"></div>
-                    </div>
-                </div>
-           
-
-</div>
-</div>
-
-
-  <!-- prueba-->          
-
-  <div class="container">
+  <div class="container-sm">
   <div class="abs-center">
     <form action="#" class="border p-3 form">
       <div class="form-group">
@@ -81,7 +31,8 @@
     </form>
   </div>
 </div>
-  <!-- prueba-->  
+   
+<!-- formulario-->  
 
 
 
