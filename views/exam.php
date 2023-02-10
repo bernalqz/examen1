e1ef45b8c1d9bef84dc25ac22a47dfbe7f209342
bernalqz <bernalqz@gmail.com>
2023-02-10 11:24:44 -0600

    s

--- views/exam.php
+++ views/exam.php
@@ -11,8 +11,8 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item mx-1"><button type="submit" class="btn btn-primary my-2">Crear la base de datos</button></li>
-                        <li class="nav-item mx-1"><button type="submit" class="btn btn-primary my-2">Borrar la base de datos</button></li>
+                        <li class="nav-item mx-1"><button type="submit" class="btn btn-primary my-2">Crear la tabla de datos</button></li>
+                        <li class="nav-item mx-1"><button type="submit" class="btn btn-primary my-2">Borrar la tabla de datos</button></li>
 
                     </ul>
                 </div>
